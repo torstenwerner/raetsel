ef1a4d2077f2187fba9f671ac9a91b0c12d11fe0
Torsten Werner <torsten.werner@westernacher.com>
2018-06-24 06:47:22 +0200

    refactor Utils

--- src/main/java/xyz/its_me/raetsel/Utils.java
+++ src/main/java/xyz/its_me/raetsel/Utils.java
@@ -8,14 +8,10 @@ import java.util.Map;
 import static java.util.stream.Collectors.toList;
 
 class Utils {
-    private static long countNullForList(List<Person> personList) {
-        return personList.stream().mapToLong(Person::countNullRelations).sum();
-    }
 
-    private static long countNullRelations(Map<Category, List<Person>> personMap) {
-        return personMap.values().stream()
-                .mapToLong(Utils::countNullForList)
-                .sum();
+    static void printRelations(Map<Category, List<Person>> personMap) {
+        personMap.values().forEach(Utils::printRelations);
+        System.out.printf("missing relation count: %d%n%n", Utils.countNullRelations(personMap));
     }
 
     private static void printRelations(List<Person> personList) {
@@ -23,10 +19,11 @@ class Utils {
         System.out.println();
     }
 
-    static void printRelations(Map<Category, List<Person>> personMap) {
-        personMap.values().forEach(Utils::printRelations);
-
-        System.out.printf("missing relation count: %d%n%n", Utils.countNullRelations(personMap));
+    private static long countNullRelations(Map<Category, List<Person>> personMap) {
+        return personMap.values().stream()
+                .flatMap(List::stream)
+                .mapToLong(Person::countNullRelations)
+                .sum();
     }
 
     static Map<Category, List<Person>> deepCopy(Map<Category, List<Person>> sourceMap) {
